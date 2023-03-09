268a6ab263ee45076515ce4cab5be2d670662884
Maung945 <myothura85@gmail.com>
2023-03-08 23:22:05 -0800

    Addeded Hybrid Method and more

--- B-N-S-Methods.cpp
+++ B-N-S-Methods.cpp
@@ -2,85 +2,98 @@
 #include <cmath>
 #include <bitset>
 #include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
 using namespace std;
 
 // BISECTIONAL METHOD
-float Bisection(float (*f)(float), float a, float b, int maxIter, float eps) {
+pair<float, int> Bisection(float (*f)(float), float a, float b, int maxIter, float eps)
+{
     float fa = f(a);
     float fb = f(b);
 
-    if (fa * fb >= 0) {
+    if (fa * fb >= 0)
+    {
         cout << "Inadequate values for a and b." << endl;
-        return -1.0;
+        return make_pair(-1.0, -1);
     }
 
     float error = b - a;
     float c = 0.0;
     float fc = 0.0;
 
-    for (int it = 1; it <= maxIter; ++it) {
+    for (int it = 1; it <= maxIter; ++it)
+    {
         error /= 2;
         c = a + error;
         fc = f(c);
 
-        if (abs(error) < eps || fc == 0) {
-            cout << "BISECTIONAL METHOD: Algorithm has converged after " << it << " iterations!" << endl;
-            return c;
+        if (abs(error) < eps || fc == 0)
+        {
+            return make_pair(c, it);
         }
 
-        if (fa * fc < 0) {
+        if (fa * fc < 0)
+        {
             b = c;
             fb = fc;
-        } else {
+        }
+        else
+        {
             a = c;
             fa = fc;
         }
     }
-
-    cout << "Max iterations reached without convergence..." << endl;
-    return c;
+    return make_pair(c, -1);;
 }
 
 // NEWTON'S METHOD
-float Newton(float (*f)(float), float (*derF)(float), float x, int maxIter, float eps, float delta) {
+pair<float, int> Newton(float (*f)(float), float (*derF)(float), float x, int maxIter, float eps, float delta)
+{
     float fx = f(x);
 
-    for (int it = 1; it <= maxIter; it++) {
+    for (int it = 1; it <= maxIter; it++)
+    {
         float fd = derF(x);
 
-        if (abs(fd) < delta) {
+        if (abs(fd) < delta)
+        {
             cout << "Small slope!" << endl;
-            return x;
+            return make_pair(x, -1);
         }
 
         float d = fx / fd;
         x -= d;
         fx = f(x);
 
-        if (abs(d) < eps) {
-            cout << "NEWTON'S METHOD: Algorithm has converged after " << it << " iterations!" << endl;
-            return x;
+        if (abs(d) < eps)
+        {
+            return make_pair(x, it);
         }
     }
-
-    cout << "Max iterations reached without convergence..." << endl;
-    return x;
+    return make_pair(x, -1);
 }
 
 // SECANT METHOD
-float Secant(float (*f)(float), float a, float b, int maxIter, float eps) {
+pair<float, int> Secant(float (*f)(float), float a, float b, int maxIter, float eps)
+{
     float fa = f(a);
     float fb = f(b);
 
-    if (std::abs(fa) > std::abs(fb)) {
-        std::swap(a, b);
-        std::swap(fa, fb);
+    if (abs(fa) > abs(fb))
+    {
+        swap(a, b);
+        swap(fa, fb);
     }
 
-    for (int it = 1; it <= maxIter; ++it) {
-        if (std::abs(fa) > std::abs(fb)) {
-            std::swap(a, b);
-            std::swap(fa, fb);
+    for (int it = 1; it <= maxIter; ++it)
+    {
+        if (abs(fa) > abs(fb))
+        {
+            swap(a, b);
+            swap(fa, fb);
         }
 
         float d = (b - a) / (fb - fa);
@@ -88,97 +101,76 @@ float Secant(float (*f)(float), float a, float b, int maxIter, float eps) {
         fb = fa;
         d = d * fa;
 
-        if (std::abs(d) < eps) {
-            std::cout << "SECANT METHOD: Algorithm has converged after " << it << " iterations!\n";
-            return a;
+        if (abs(d) < eps)
+        {
+            return make_pair(a, it);
         }
-
         a = a - d;
         fa = f(a);
     }
-
-    std::cout << "Maximum number of iterations reached!\n";
-    return a;
+    cout << "Maximum number of iterations reached!\n";
+    return make_pair(a, -1);
 }
 
-// Define the function f(x) = x^3 - x - 1 // f(x) = 3x^3 + 5x^2 - 7
-float f(float x) {
-    //return 3 * pow(x, 3) + 5 * pow(x, 2) - 0 * pow(x, 1) - 7;
-    //return pow(x, 3) + 3*x - 1;
-    return pow(x, 3) + 2 * pow(x, 2) + 10 * pow(x, 1) - 20;
-}
-
-// Differentiated Function.
-float derF(float x) {
-    //return 2*x;
-    return 2 * pow(x, 2) + 4 * pow(x, 1) + 10;
-}
-/*
-//HYBRID
-float Hybrid(float (*f)(float), float a, float b, int maxIter, float eps) {
-    float fa = f(a);
-    float fb = f(b);
-
-    if (fa * fb >= 0) {
-        cout << "Inadequate values for a and b." << endl;
-        return -1.0;
-    }
-
-    float error = b - a;
-    float c, fc;
-
-    for (int it = 1; it <= maxIter; it++) {
-        error = error / 2;
-        c = a + error;
-        fc = f(c);
-
-        if (fabs(error) < eps || fc == 0) {
-            cout << "Algorithm has converged after " << it << " iterations!" << endl;
-            return c;
-        }
-
-        if (fa * fc < 0) {
-            b = c;
-            fb = fc;
-        }
-        else {
-            a = c;
-            fa = fc;
-        }
-        
-        // After 5 iterations, switch to Newton's Method
-        if (it == 5) {
-            cout << "Switching to Newton's Method..." << endl;
-            return Newton(f, derF, c, maxIter - 5, eps, eps);
-        }
-    }
-
-    cout << "Max iterations reached without convergence..." << endl;
-    return c;
-}
-*/
-
-float Hybrid(float (*f)(float), float (*derF)(float), float a, float b, int maxIter, float eps, float delta) {
-    double x = Bisection(f, a, b, 3, eps);
-    x = Newton(f, derF, x, maxIter - 3, eps, delta);
+pair<float, int> Hybrid(float (*f)(float), float (*derF)(float), float a, float b, int maxIter, float eps, float delta)
+{
+    pair<float, int> x = Bisection(f, a, b, 3, eps);
+    x = Newton(f, derF, x.first, maxIter - 3, eps, delta);
     return x;
 }
 
-int main() {
-    float a = 1.0;
-    float b = 2.0;
-    int maxIter = 10000;                               // maximum number of iterations
-    float eps = 1e-6;                                  // tolerance for convergence
-    float delta = 1e-6;                                // tolerance for small slope
-    
-    float root_by_bisection = Bisection(f, a, b, maxIter, eps);
-    float root_by_newton = Newton(f, derF, b, maxIter, eps, delta);
-    float root_by_secant = Secant(f, a, b, maxIter, eps);
-    cout << "Root found at x (By Bisectiona Method.) = " << setprecision(32) << root_by_bisection << endl;
-    cout << "Root found at x (By Newton's Method.) = " << setprecision(32) << root_by_newton << endl;
-    cout << "Root found at x (By Secant's Method.) = " << setprecision(32) << root_by_newton << endl;
-    
-   float root_by_hybrid= Hybrid(f, derF, a, b, maxIter, eps, delta);
-   cout << "Root found at x (By Hybrid Method.) = " << setprecision(32) << root_by_hybrid << endl;
+// Define the function f(x) = x^3 - x - 1 // f(x) = 3x^3 + 5x^2 - 7
+float f(float x)
+{
+    ifstream inputFile("fun1.pol");
+    int n;
+    float result;
+    vector<float> a(4);
+    if (inputFile.is_open())
+    {
+        string line;
+        getline(inputFile, line);
+
+        // Read the first integer
+        stringstream(line) >> n;
+        for (int i = 0; i < 4; i++)
+        {
+            inputFile >> a[i];
+        }
+    }
+    else
+    {
+        cerr << "Unable to open input file" << std::endl;
+    }
+    return (a[0] * pow(x, 3) + a[1] * pow(x, 2) + a[2] * pow(x, 1) + a[3]);
+}
+
+// Differentiated Function.
+float derF(float x)
+{
+    // return 9 * pow(x, 2) + 10 * pow(x, 1);
+    return 2 * pow(x, 2) + 4 * pow(x, 1) + 10;
+}
+
+int main()
+{
+    float a = 2.0;
+    float b = 1.0;
+    int maxIter = 10000;            // maximum number of iterations
+    float eps = 1e-6;               // tolerance for convergence
+    float delta = 1e-6;             // tolerance for small slope
+    pair<float, int> root_by_bisection = Bisection(f, a, b, maxIter, eps);
+    pair<float, int> root_by_newton = Newton(f, derF, b, maxIter, eps, delta);
+    pair<float, int> root_by_secant = Secant(f, a, b, maxIter, eps);
+    pair<float, int> root_by_hybrid = Hybrid(f, derF, a, b, maxIter, eps, delta);
+    cout << "Solutions are being saved in \"solution.sol\" file!!"  << endl;
+    ofstream outputFile;
+    outputFile.open("solution.sol");
+    outputFile << "Method\t\tRoot\t\t\t\t\t\tIterations\t\tOutcome" << endl;
+    outputFile << "Bisection\t" << setw(30) << left << setprecision(32) << root_by_bisection.first << setw(9) << root_by_bisection.second << "\t\tSuccess!" << endl;
+    outputFile << "Newton's \t" << setw(30) << left << setprecision(32) << root_by_newton.first << setw(9) << root_by_newton.second << "\t\tSuccess!" << endl;
+    outputFile << "Secant   \t" << setw(30) << left << setprecision(32) << root_by_secant.first << setw(9) << root_by_secant.second << "\t\tSuccess!" << endl;
+    outputFile << "Hybrid   \t" << setw(30) << left << setprecision(32) << root_by_hybrid.first << setw(9) << root_by_hybrid.second << "\t\tSuccess!" << endl;
+    outputFile.close();
     return 0;
 }
---
+++ solution.sol
@@ -0,0 +1,5 @@
+Method		Root						Iterations		Outcome
+Bisection	1.36880779266357421875        20       		Success!
+Newton's 	1.36880815029144287109375     7        		Success!
+Secant   	1.36880779266357421875        5        		Success!
+Hybrid   	1.3688080310821533203125      5        		Success!
